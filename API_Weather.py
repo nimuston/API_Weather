c0a6a46881d8625faa699231e61e3b0db33443cc
Niko Mustonen <niko.mustonen@saunalahti.fi>
2022-04-18 13:14:59 +0300

    Simplified code

--- API_Weather.py
+++ API_Weather.py
@@ -13,21 +13,18 @@ def main(api_key, location):
     if responce.status_code == 200:
         print("Connection to API was successfull, start fecthing weather information from " + location)
         responce_to_dict = responce.json()
-        request = responce_to_dict['request']
-        location = responce_to_dict['location']
-        current = responce_to_dict['current']
-        print("Location which weather information is fetched: " +location['name'])
-        print("Location country: " +location['country'])
-        print("Locations latitude: " +location['lat'] + " " + " and longitude: " + location['lon'])
-        print("Current temperature: " +str(current['temperature']) +" c")
-        print("Weather description: " +str(current['weather_descriptions']))
-        print("Wind speed is: " +str(current['wind_speed']))
+        print("Location which weather information is fetched: " +responce_to_dict['location']['name'])
+        print("Location country: " +responce_to_dict['location']['country'])
+        print("Locations latitude: " +responce_to_dict['location']['lat'] + " " + " and longitude: " + responce_to_dict['location']['lon'])
+        print("Current temperature: " +str(responce_to_dict['current']['temperature']) +" c")
+        print("Weather description: " +str(responce_to_dict['current']['weather_descriptions']))
+        print("Wind speed is: " +str(responce_to_dict['current']['wind_speed']))
     else:
         print("Something went wrong, please check API key")
 
 
 if __name__ == "__main__":
     print("Give API_key as first argument, and city as second argument")
-    api_key = str(sys.argv[1])
-    location = str(sys.argv[2])
+    api_key = input("Please give API key: ")
+    location = input("Please give location which weather details you what: ")
     main(api_key, location)
